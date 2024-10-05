c13400c50078f7e798ed9f4d2e3dee6d58f2db7f
unknown <edward.cruzn@gmail.com>
2024-10-05 11:36:30 -0500

    feat: Se crea el proyecto de ejemplo para aprender C#

---
+++ Fundamentos/Proyecto1/Index.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Proyecto1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proyecto1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mostrarMensaje();
+
+        }
+        //Metodo
+        //Crear un metodo publico, los metodos deben comenzar por verbos
+        public void mostrarMensaje()
+        {
+            //Crear un objeto de Car
+            Car objCar = new Car();
+            //Creo una variable local de tipo string
+            string msj = objCar.stop();
+            LblMensaje.Text = msj;
+
+        }
+            
+
+    }
+}
